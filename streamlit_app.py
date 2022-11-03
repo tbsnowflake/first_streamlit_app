7bdf1a265c2bf407e788fec258a8e0c41a341f00
tbsnowflake <117375766+tbsnowflake@users.noreply.github.com>
2022-11-03 15:49:53 -0400

    Update streamlit_app.py
    process and display json

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
+
+# normalize json response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display normalized json as table
+streamlit.dataframe(fruityvice_normalized)
